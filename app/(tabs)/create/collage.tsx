3c8d4fc4e30ceadd50a46b23851d252e102c3ace
Raminx524 <Raminx524@gmail.com>
2024-09-03 20:18:06 +0300

    Added CreateModal + CreateBoard

---
+++ app/(tabs)/create/collage.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const CreateCollage = () => {
+  return (
+    <View>
+      <Text>CreateCollage</Text>
+    </View>
+  );
+};
+
+export default CreateCollage;
